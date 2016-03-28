021da8462b331d6db46133bbc939fbf880a35324
tomasdepi <tomas94depi@gmail.com>
2016-03-27 22:01:03 -0300

    Cambio Version JUnit

--- src/test/java/ar/edu/utn/dds/astilla/macowins/PantalonTest.java
+++ src/test/java/ar/edu/utn/dds/astilla/macowins/PantalonTest.java
@@ -1,16 +1,22 @@
 package ar.edu.utn.dds.astilla.macowins;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
 import junit.framework.Assert;
 
+@SuppressWarnings("deprecation")
 public class PantalonTest {
 	
 	private Pantalon unPantalon;
 	
+	@Before
 	public void init() {
 		unPantalon = new Pantalon(new Internacional());
 	}
 	
+	@Test
 	public void precioBaseEsDoscientosCincuenta(){
 		Assert.assertEquals(250, unPantalon.precioBase());
 	}
