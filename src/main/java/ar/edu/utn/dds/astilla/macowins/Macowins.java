cab03a34a11b324343f99af2cfc92c7cb3288ebb
jlusnich <juanma.lusnich@gmail.com>
2016-04-01 01:20:14 -0300

    Actualización del POM - Otros cambios menores

--- src/main/java/ar/edu/utn/dds/astilla/macowins/Macowins.java
+++ src/main/java/ar/edu/utn/dds/astilla/macowins/Macowins.java
@@ -1,27 +1,13 @@
 package ar.edu.utn.dds.astilla.macowins;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Macowins {
 
 static List<Venta> ventas= new ArrayList<Venta>() ;
 	
-	public static void main(String[] args) {
-		
-		Pantalon unPantalon = new Pantalon(new Internacional());
-		Saco unSaco = new Saco(new Nacional());
-		Camisa unaCamisa = new Camisa(new Internacional());
-		
-		vender(unPantalon,10,12);
-		vender(unSaco,3,10);
-		vender(unaCamisa,2,12);
-				
-		
-		System.out.println(procesarVentasDelDia(12));
-		
-	}
-	
-	public static int procesarVentasDelDia(long fecha){
+	public static double procesarVentasDelDia(long fecha){
 		return ventas.stream().filter(venta -> venta.coincideFecha(fecha)).mapToDouble(venta -> venta.ganancia()).sum();
 	}
 	
